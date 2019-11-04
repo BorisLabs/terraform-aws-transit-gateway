c892a7bb99bbb36949b248f881983d0abb464b8e
Josh Sinfield <joshuasinfield@googlemail.com>
2019-11-04 12:33:56 +0000

    Update module to be Terraform 12
    Closes #1

--- data.tf
+++ data.tf
@@ -1,11 +1,13 @@
-data "aws_caller_identity" "this" {}
+data "aws_caller_identity" "this" {
+}
 
 data "aws_vpc" "default" {
-  default = "${var.vpc_id == "" ? true : false}"
-  id      = "${var.vpc_id}"
+  default = var.vpc_id == "" ? true : false
+  id      = var.vpc_id
 }
 
 data "aws_subnet_ids" "subnets" {
-  count  = "${var.vpc_id == "" ? 1 : 0}"
-  vpc_id = "${data.aws_vpc.default.id}"
+  count  = var.vpc_id == "" ? 1 : 0
+  vpc_id = data.aws_vpc.default.id
 }
+
